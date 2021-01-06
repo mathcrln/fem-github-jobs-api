c7dcd030c937b64d467cc8c25eba25813c38c398
Mathieu <ceraline.mathieu@gmail.com>
2021-01-06 00:12:58 -0500

    Theme improvements
    Updates the CSS theme and the global stylesheet to simplify it.

--- global.js
+++ global.js
@@ -9,7 +9,8 @@ export const GlobalStyles = createGlobalStyle`
 
   html,
   body {
-
+    margin: 0;
+    padding: 0;
   }
   
   body {
--- theme.js
+++ theme.js
@@ -1,10 +1,8 @@
 export const lightTheme = {
 	body: '#F4F6F8',
 	text: '#363537',
-	toggleBorder: '#FFF',
 	offerBackground: '#FFFFFF',
 	headingColor: '#19202D',
-	gradient: 'linear-gradient(#39598A, #79D7ED)',
 };
 
 export const darkTheme = {
@@ -12,6 +10,4 @@ export const darkTheme = {
 	text: '#FAFAFA',
 	offerBackground: '#19202D',
 	headingColor: '#FFFFFF',
-	toggleBorder: '#6B8096',
-	gradient: 'linear-gradient(#091236, #1E215D)',
 };
