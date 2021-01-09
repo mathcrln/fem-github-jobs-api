d97edd2c162b90dab7a136806104e14ad7a48cad
Mathieu <ceraline.mathieu@gmail.com>
2021-01-08 22:19:36 -0500

    Checkbox Component

---
+++ components/Checkbox/Checkbox.jsx
@@ -0,0 +1,18 @@
+import {
+	HiddenCheckbox,
+	StyledCheckbox,
+	CheckboxContainer,
+	Icon,
+} from './checkbox.styled';
+
+const Checkbox = ({ className, checked, ...props }) => (
+	<CheckboxContainer className={className}>
+		<HiddenCheckbox checked={checked} {...props} />
+		<StyledCheckbox checked={checked}>
+			<Icon viewBox='0 0 24 24'>
+				<polyline points='20 6 9 17 4 12' />
+			</Icon>
+		</StyledCheckbox>
+	</CheckboxContainer>
+);
+export default Checkbox;
--- theme.js
+++ theme.js
@@ -2,6 +2,7 @@ export const lightTheme = {
 	body: '#F4F6F8',
 	text: '#363537',
 	elementsBackground: '#FFFFFF',
+	checkBackground: '#E7E8E9',
 	headingColor: '#19202D',
 };
 
@@ -9,5 +10,7 @@ export const darkTheme = {
 	body: '#121721',
 	text: '#FAFAFA',
 	elementsBackground: '#19202D',
+	checkBackground: '#313743',
+
 	headingColor: '#FFFFFF',
 };
